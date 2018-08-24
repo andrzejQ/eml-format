656bdd10176a5bd5d85cb82307c4e9cf679c8af2
AQ <kubaszek@prz.edu.pl>
2018-08-24 20:35:11 +0200

    in eml-format.js: - return new Buffer( + return Buffer.from(
    mbox-example.js:
    errors log improvements

--- tmp/mbox-example.js
+++ tmp/mbox-example.js
@@ -1,7 +1,10 @@
+"use strict";
+
 var emlformat     = require('../lib/eml-format.js');
 const Mbox        = require('node-mbox');
 const mbox        = new Mbox();
 var i = 0;
+var err = '';
 // wait for message events
 mbox.on('message', function(eml) {
   var num = String(++i);
@@ -9,10 +12,18 @@ mbox.on('message', function(eml) {
   num += '.';
   console.log(num+">>>");
   emlformat.unpack2(eml.toString("binary"), './em',num, function(error, data) {
-    if (error) return console.log(error);
-    console.log(data); //List of files
-    console.log(num+" saved **************************************************");
+    if (error) 
+      err += num+" !!!\n"+error;
+    else {
+      console.log(data); //List of files
+      console.log(num+" saved **************************************************");
+    }
   });
 });
+
+mbox.on('end', function() {
+  if (err) console.log('\nERRORS:\n'+err);
+});
+
 // pipe stdin to mbox parser: node mbox-example.js <../test/fixtures/my.mbox
 process.stdin.pipe(mbox);
