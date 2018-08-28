b7e27d42ef42163c88dbbbead9837ad94264b877
Andrzej Kubaszek <kubaszek@prz.edu.pl>
2018-08-28 15:15:41 +0200

    test-iconv-thin.js

---
+++ test/iconv-thin_decode.js
@@ -0,0 +1,52 @@
+"use strict";
+
+var iconv = require('../lib/iconv-thin');
+
+var asciiString='\u0000\u0001\u0002\u0003\u0004\u0005\u0006\u0007\b\t\n\u000b\f\r\u000e\u000f\u0010\u0011\u0012\u0013\u0014\u0015\u0016\u0017\u0018\u0019\u001a\u001b\u001c\u001d\u001e\u001f !"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~'; 
+var sbcs_data_generated = {
+  "windows1250": {
+    "type": "_sbcs",
+    "chars": "€�‚�„…†‡�‰Š‹ŚŤŽŹ�‘’“”•–—�™š›śťžź ˇ˘Ł¤Ą¦§¨©Ş«¬­®Ż°±˛ł´µ¶·¸ąş»Ľ˝ľżŔÁÂĂÄĹĆÇČÉĘËĚÍÎĎĐŃŇÓÔŐÖ×ŘŮÚŰÜÝŢßŕáâăäĺćçčéęëěíîďđńňóôőö÷řůúűüýţ˙"
+  },
+  "windows1251": {
+    "type": "_sbcs",
+    "chars": "ЂЃ‚ѓ„…†‡€‰Љ‹ЊЌЋЏђ‘’“”•–—�™љ›њќћџ ЎўЈ¤Ґ¦§Ё©Є«¬­®Ї°±Ііґµ¶·ё№є»јЅѕїАБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдежзийклмнопрстуфхцчшщъыьэюя"
+  },
+  "windows1252": {
+    "type": "_sbcs",
+    "chars": "€�‚ƒ„…†‡ˆ‰Š‹Œ�Ž��‘’“”•–—˜™š›œ�žŸ ¡¢£¤¥¦§¨©ª«¬­®¯°±²³´µ¶·¸¹º»¼½¾¿ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖ×ØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõö÷øùúûüýþÿ"
+  },
+  "windows1253": {
+    "type": "_sbcs",
+    "chars": "€�‚ƒ„…†‡�‰�‹�����‘’“”•–—�™�›���� ΅Ά£¤¥¦§¨©�«¬­®―°±²³΄µ¶·ΈΉΊ»Ό½ΎΏΐΑΒΓΔΕΖΗΘΙΚΛΜΝΞΟΠΡ�ΣΤΥΦΧΨΩΪΫάέήίΰαβγδεζηθικλμνξοπρςστυφχψωϊϋόύώ�"
+  }
+}
+
+var utf = "这是中文字符测试。。！@￥%12abcABC‘’“”•–—™š›śťžź ˇ˘Ł¤Ą¦§¨©Ş«¬­®Ż°±˛ł´µ¶·¸ąş»Ľ˝ľżŔÁÂĂÄĹĆÇČÉĘËĚÍÎĎĐŃŇÓÔŐÖ×ŘŮÚŰÜÝŢßŕáâăäĺćçčéęëěíîďđńňóôőö÷řůúűüýţ˙";
+
+var ansiBuf = function () {
+  var newBuf = Buffer.alloc(0x100);
+  for (var i = 0; i < newBuf.length; i++)
+    newBuf[i] = i;
+  return newBuf;
+} ();
+var out = {};
+
+exports["iconv-thin.decode()"] = function(test) {
+  var expected, actual;
+  for (var key in sbcs_data_generated) {
+    if (sbcs_data_generated.hasOwnProperty(key)) {
+      expected = asciiString + sbcs_data_generated[key].chars;
+      actual = iconv.decode(Buffer.from(ansiBuf), key);
+      test.ok(actual == expected, key+': Expected "' + expected + '" but got "' + actual + '"');
+      actual = iconv.decode(Buffer.from(ansiBuf), key.replace(/[\D]/g,''));
+      test.ok(actual == expected, key+': Expected "' + expected + '" but got "' + actual + '"');
+    }
+  }
+  var key = 'utf8';
+  expected = utf;
+  actual = iconv.decode(utf, key);
+  test.ok(actual == expected, key+': Expected "' + expected + '" but got "' + actual + '"');
+  
+  test.done();
+};
