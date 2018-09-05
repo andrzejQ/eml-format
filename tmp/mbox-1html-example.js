b44f1ba058f3e6145dcb5c37972e78b1f66be965
Andrzej Kubaszek <kubaszek@prz.edu.pl>
2018-09-05 14:33:01 +0200

    mbox-1html-example.js All emails in 1 HTML file with linked attachments.

---
+++ tmp/mbox-1html-example.js
@@ -0,0 +1,66 @@
+"use strict";
+/***********************
+All emails in 1 HTML file with linked attachments.
+*/
+var emlformat     = require('../lib/eml-format.js');
+var fs = require("fs");
+var path = require("path");
+const Mbox        = require('node-mbox');
+const mbox        = new Mbox();
+
+emlformat.allTxtToHtml = true; //unpack2() - save plain text file as HTML with '<br>' at line break
+emlformat.htmlBodyStr = true; // set `data.data_html_body` as string of each html body
+emlformat.charsetDefault = 'iso-8859-1';
+
+var i = 0;
+var err = '';
+var directory = './em';
+var fName = 'All.html';
+fs.writeFileSync(path.resolve(directory, fName), `<!doctype html><html><meta charset="UTF-8" />
+<head>
+<style>
+div.FromTo {background-color:LightBlue;}
+div.FileList {background-color:Aqua; margin:2px 0px;}
+div.FromTo ul, div.FileList ul {margin: 0px; padding:4px;}
+div.FromTo ul {list-style: none;}
+div.FileList li{display:inline; padding: 0px 6px;}
+.plainText {font-family: monospace;}
+.nextEmail {background-color:Aqua;}
+</style>
+</head><html><body>`
+);
+
+// wait for message events
+mbox.on('message', function(eml) {
+  var num = String(++i);
+  if (num.length < 5) num = String('00000'+num).slice(-5); //'00001'
+  num += '.';
+  console.log(num+">>>");
+  fs.appendFileSync(path.resolve(directory, fName), `
+  <br>
+  <br>
+  <hr>
+  <h2 class="nextEmail">`+num+`</h2>
+  <hr>
+  
+  `);
+  emlformat.unpack2(eml.toString("binary"), directory,num, function(error, data) {
+    if (error) 
+      err += num+" !!!\n"+error;
+    else {
+      if (data.files[0]) {console.log(data.files)}; //List of attachments
+      //console.log(data.data_html_body);
+     fs.appendFileSync(path.resolve(directory, fName), data.data_html_body);
+
+      console.log(data.indxs+"-body added **************************************************");
+    }
+  });
+});
+
+mbox.on('end', function() {
+  fs.appendFileSync(path.resolve(directory, fName), '</body></html>');
+  if (err) console.log('\nERRORS:\n'+err);
+});
+
+// pipe stdin to mbox parser: node mbox-example.js <../test/fixtures/my.mbox
+process.stdin.pipe(mbox);
